8e0fb35fadc260643e9691cb3cd888bc7246be9e
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2024-10-28 11:26:20 +0530

    Update Hero.tsx

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -171,7 +171,8 @@ const Hero: React.FC = () => {
               className="w-full flex scale-[.25] sm:scale-[.35] lg:scale-[.5] items-center justify-center"
               style={{ overflow: "visible" }}
               // scene="https://prod.spline.design/ZiqyYsBzgvZY6PTk/scene.splinecode"
-              scene="https://prod.spline.design/5km77ydj1V7-CpvQ/scene.splinecode"
+              // scene="https://prod.spline.design/5km77ydj1V7-CpvQ/scene.splinecode"
+              scene="https://prod.spline.design/hzePL2w2MjAF8Zrh/scene.splinecode"
               onLoad={onSplineLoad}
             />
           </motion.div>
