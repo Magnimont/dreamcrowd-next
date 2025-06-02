8cd423443a0b9d440c57b6469be3135196b8675a
eritaakash <eritaakash@gmail.com>
2025-06-02 15:14:42 +0530

    remove other sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import Hero from "@/components/home/Hero";
 import NavbarComponent from "@/components/common/NavBar";
-import Features from "@/components/sections/Features";
-import Pricing from "@/components/sections/Pricing";
-import Testimonials from "@/components/sections/Testimonials";
-import FAQ from "@/components/sections/FAQ";
 
 export default function Home() {
   return (
     <>
       <NavbarComponent />
       <Hero />
-      <Features />
-      <Pricing />
-      <Testimonials />
-      <FAQ />
     </>
   );
 }
